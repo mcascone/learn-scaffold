d9d76cc07948a04c3c9e93fdf6e2089e9ada1e3c
mcascone <max@bitovi.com>
2023-05-08 15:41:56 -0500

    safely wait until db is available, remove depends_on

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 
 import Koa from "koa"
 import dotenv from 'dotenv'
+dotenv.config();
 
 import { Scaffold, DataTypes } from "bitscaffold";
 import { Player, Team } from "./Models.js";
-
-dotenv.config();
 
 const User = {
   name: "User",
@@ -18,30 +17,49 @@ const User = {
 };
 
 const app = new Koa();
+const port = process.env.API_PORT || 3000;
 
-const scaffold = new Scaffold([Player, Team, User], {
-  name: "Scaffold Demo",
-  prefix: "/api",
-  database: {
-    dialect: "postgres",
-    host: process.env.DB_HOST ?? 'localhost',
-    port: Number(process.env.DB_PORT) ?? 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    logging: false
-  },
-});
+// await scaffold.createDatabase();
+async function waitForConnection(scaffold) {
+  let isConnected = false;
 
-app.use(scaffold.middleware.allModels.all);
+  while (!isConnected) {
+      try {
+          await scaffold.createDatabase();
+          isConnected = true;
+      } catch (error) {
+          console.log('Database connection not available yet. Retrying in 5 seconds...');
+          await new Promise(resolve => setTimeout(resolve, 5000));
+      }
+  }
 
-await scaffold.createDatabase();
+  console.log('Database connection established.');
+}
+
+// Usage
+(async () => {
+  const scaffold = new Scaffold([Player, Team, User], {
+    name: "Scaffold Demo",
+    prefix: "/api",
+    database: {
+      dialect: "postgres",
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT) ?? 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      logging: false
+    },
+  });
+  
+  app.use(scaffold.middleware.allModels.all);
+  await waitForConnection(scaffold);
+})();
+
 
 app.use(async (ctx) => {
     ctx.body = "Hello From Koa";
 });
-
-const port = process.env.API_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
